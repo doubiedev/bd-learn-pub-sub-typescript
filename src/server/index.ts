510c8e487af6e9fd769420932d4b83ce57a40179
Nicholas Doublet <thedoubiedev@gmail.com>
2026-01-20 12:19:11 +1100

    match 2.4 solution

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,17 +1,26 @@
 import amqp from "amqplib";
 
 async function main() {
-    console.log("Starting Peril server...");
-    const connectionString = "amqp://guest:guest@localhost:5672/";
-    const conn = await amqp.connect(connectionString);
-    console.log("Connected to AMQP server.");
-    process.on("SIGINT", () => {
-        console.log("Shutting down...");
-        conn.close();
-    });
+    const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+    const conn = await amqp.connect(rabbitConnString);
+    console.log("Peril game server connected to RabbitMQ!");
+
+    ["SIGINT", "SIGTERM"].forEach((signal) =>
+        process.on(signal, async () => {
+            try {
+                await conn.close();
+                console.log("RabbitMQ connection closed.");
+            } catch (err) {
+                console.error("Error closing RabbitMQ connection:", err);
+            } finally {
+                process.exit(0);
+            }
+        }),
+    );
 }
 
 main().catch((err) => {
     console.error("Fatal error:", err);
     process.exit(1);
 });
+
