4bbda8ea5e99a31c8c1accfb3bf6d990a781744a
Nicholas Doublet <thedoubiedev@gmail.com>
2026-01-20 11:42:34 +1100

    complete 2.4

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,17 @@
+import amqp from "amqplib";
+
 async function main() {
-  console.log("Starting Peril server...");
+    console.log("Starting Peril server...");
+    const connectionString = "amqp://guest:guest@localhost:5672/";
+    const conn = await amqp.connect(connectionString);
+    console.log("Connected to AMQP server.");
+    process.on("SIGINT", () => {
+        console.log("Shutting down...");
+        conn.close();
+    });
 }
 
 main().catch((err) => {
-  console.error("Fatal error:", err);
-  process.exit(1);
+    console.error("Fatal error:", err);
+    process.exit(1);
 });
