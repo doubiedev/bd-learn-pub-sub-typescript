6a100765db5642bd64757901148ec2c74d6b55bc
Nicholas Doublet <thedoubiedev@gmail.com>
2026-01-21 13:32:57 +1100

    match 3.1 solution

--- src/internal/pubsub/publish.ts
+++ src/internal/pubsub/publish.ts
@@ -1,12 +1,26 @@
-import type { ConfirmChannel } from "amqplib";
+import { type ConfirmChannel } from "amqplib";
 
-export async function publishJSON<T>(
+export function publishJSON<T>(
     ch: ConfirmChannel,
     exchange: string,
     routingKey: string,
     value: T,
 ): Promise<void> {
-    const json = JSON.stringify(value);
-    const content = Buffer.from(json);
-    ch.publish(exchange, routingKey, content, { contentType: "application/json" });
-};
+    const content = Buffer.from(JSON.stringify(value));
+
+    return new Promise((resolve, reject) => {
+        ch.publish(
+            exchange,
+            routingKey,
+            content,
+            { contentType: "application/json" },
+            (err) => {
+                if (err !== null) {
+                    reject(new Error("Message was NACKed by the broker"));
+                } else {
+                    resolve();
+                }
+            },
+        );
+    });
+}
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,17 +1,11 @@
 import amqp from "amqplib";
 import { publishJSON } from "../internal/pubsub/publish.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
-import type { PlayingState } from "../internal/gamelogic/gamestate.js";
 
 async function main() {
     const rabbitConnString = "amqp://guest:guest@localhost:5672/";
     const conn = await amqp.connect(rabbitConnString);
     console.log("Peril game server connected to RabbitMQ!");
-
-    const ch = await conn.createConfirmChannel();
-    const data = { isPaused: true } satisfies PlayingState;
-
-    publishJSON(ch, ExchangePerilDirect, PauseKey, data);
 
     ["SIGINT", "SIGTERM"].forEach((signal) =>
         process.on(signal, async () => {
@@ -25,6 +19,16 @@ async function main() {
             }
         }),
     );
+
+    const publishCh = await conn.createConfirmChannel();
+
+    try {
+        await publishJSON(publishCh, ExchangePerilDirect, PauseKey, {
+            isPaused: true,
+        });
+    } catch (err) {
+        console.error("Error publishing message:", err);
+    }
 }
 
 main().catch((err) => {
