d729a20e4e19ff7c055a704670ade70fac3c8861
Nicholas Doublet <thedoubiedev@gmail.com>
2026-01-22 13:43:54 +1100

    complete 3.11

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,13 @@
 import amqp from "amqplib";
 import { publishJSON } from "../internal/pubsub/publish.js";
-import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import {
+    ExchangePerilDirect,
+    ExchangePerilTopic,
+    GameLogSlug,
+    PauseKey,
+} from "../internal/routing/routing.js";
 import { getInput, printServerHelp } from "../internal/gamelogic/gamelogic.js";
+import { declareAndBind, SimpleQueueType } from "../internal/pubsub/consume.js";
 
 async function main() {
     const rabbitConnString = "amqp://guest:guest@localhost:5672/";
@@ -22,6 +28,14 @@ async function main() {
     );
 
     const publishCh = await conn.createConfirmChannel();
+
+    declareAndBind(
+        conn,
+        ExchangePerilTopic,
+        GameLogSlug,
+        `${GameLogSlug}.*`,
+        SimpleQueueType.Durable,
+    );
 
     printServerHelp();
 
@@ -48,8 +62,6 @@ async function main() {
             } catch (err) {
                 console.error("Error publishing resume message:", err);
             }
-        } else if (command === "help") {
-            printServerHelp();
         } else if (command === "quit") {
             console.log("Goodbye!");
             process.exit(0);
