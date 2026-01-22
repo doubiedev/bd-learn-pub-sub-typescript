4c84b37f0b04b23e5cb1a51a05730fd1f723eca4
Nicholas Doublet <thedoubiedev@gmail.com>
2026-01-22 13:34:46 +1100

    match 3.7 solution

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,11 +1,16 @@
 import amqp from "amqplib";
-import { isValidLocation, isValidRank } from "../internal/gamelogic/gamedata.js";
-import { clientWelcome, commandStatus, getInput, printClientHelp, printQuit } from "../internal/gamelogic/gamelogic.js";
-import { GameState } from "../internal/gamelogic/gamestate.js";
-import { commandMove } from "../internal/gamelogic/move.js";
-import { commandSpawn } from "../internal/gamelogic/spawn.js";
+import {
+    clientWelcome,
+    commandStatus,
+    getInput,
+    printClientHelp,
+    printQuit,
+} from "../internal/gamelogic/gamelogic.js";
 import { declareAndBind, SimpleQueueType } from "../internal/pubsub/consume.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import { GameState } from "../internal/gamelogic/gamestate.js";
+import { commandSpawn } from "../internal/gamelogic/spawn.js";
+import { commandMove } from "../internal/gamelogic/move.js";
 
 async function main() {
     const rabbitConnString = "amqp://guest:guest@localhost:5672/";
@@ -35,28 +40,38 @@ async function main() {
         SimpleQueueType.Transient,
     );
 
-    const gameState = new GameState(username);
+    const gs = new GameState(username);
 
     while (true) {
         const words = await getInput();
-        if (words.length === 0) continue;
-
+        if (words.length === 0) {
+            continue;
+        }
         const command = words[0];
-        if (command === "spawn") {
-            commandSpawn(gameState, words);
-        } else if (command === "move") {
-            commandMove(gameState, words);
+        if (command === "move") {
+            try {
+                commandMove(gs, words);
+            } catch (err) {
+                console.log((err as Error).message);
+            }
         } else if (command === "status") {
-            commandStatus(gameState);
+            commandStatus(gs);
+        } else if (command === "spawn") {
+            try {
+                commandSpawn(gs, words);
+            } catch (err) {
+                console.log((err as Error).message);
+            }
         } else if (command === "help") {
-            printClientHelp()
+            printClientHelp();
+        } else if (command === "quit") {
+            printQuit();
+            process.exit(0);
         } else if (command === "spam") {
             console.log("Spamming not allowed yet!");
-        } else if (command === "quit") {
-            printQuit()
-            break;
         } else {
             console.log("Unknown command");
+            continue;
         }
     }
 }
@@ -65,4 +80,3 @@ main().catch((err) => {
     console.error("Fatal error:", err);
     process.exit(1);
 });
-
